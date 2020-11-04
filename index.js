505d0a47294e20ae93a0696cfcdbbb0ec9779574
Cmac <mckoyc@gmail.com>
2020-11-03 21:51:56 -0500

    added questions to question array and required Inquirer in js file

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
+const inquirer = require('inquirer')
+
 // array of questions for user
 const questions = [
-
+    `What is the name of your project?`, 
+    `Briefly describe your project`,
+    `Does your package require any installation?`,
+    `What needs to be installed?`,
+    `How does your project work?`,
+    `What License does it use?`,
+    `How can someone contribute to this project?`,
+    'How do you test your project?',
+    `What is your Github URL?`,
+    `What is your email?`
 ];
 
 // function to write README file
