63aff3f01dd530a92a8a1d0a87d6a972e9d657fe
Cmac <mckoyc@gmail.com>
2020-11-06 17:07:59 -0500

    changed fs.writeFile to fs.writeFileSync to fix problem with not generating md file

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Link Inquirer to JS File
 const fs = require('fs');
-const inquirer = require('inquirer')
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // array of questions for user
 const questions = [{
@@ -11,42 +12,46 @@ const questions = [{
     type: 'input',
     message:`Briefly describe your project`,
     name: 'description'
-},{
-    type: 'input',
-    message: `What needs to be installed?`,
-    name: 'install'
-    
-},{
-    type: 'input',
-    message:`How does your project work?`,
-    name: 'usage'
-},{
-    type: 'list',
-    message: `What License does it use?`,
-    choices: ['MIT', 'Apache', 'GNU', 'Mozilla', 'IBM', "ISC"],
-    name: 'license'
-},{
-    type: 'input',
-    message:`How can someone contribute to this project?`,
-    name: 'contact'
-},{
-    type: 'input',
-    message:'How do you test your project?',
-    name: 'test'
-},{
-    type: 'input',
-    message:`What is your Github URL?`,
-    name: 'github'
-},{
-    type: 'input',
-    message:`What is your email?`,
-    name: 'email'
 }
+    // },{
+//     type: 'input',
+//     message: `What needs to be installed?`,
+//     name: 'install'
+    
+// },{
+//     type: 'input',
+//     message:`How does your project work?`,
+//     name: 'usage'
+// },{
+//     type: 'list',
+//     message: `What License does it use?`,
+//     choices: ['MIT', 'Apache', 'GNU', 'Mozilla', 'IBM', "ISC"],
+//     name: 'license'
+// },{
+//     type: 'input',
+//     message:`How can someone contribute to this project?`,
+//     name: 'contact'
+// },{
+//     type: 'input',
+//     message:'How do you test your project?',
+//     name: 'test'
+// },{
+//     type: 'input',
+//     message:`What is your Github URL?`,
+//     name: 'github'
+// },{
+//     type: 'input',
+//     message:`What is your email?`,
+//     name: 'email'
+// }
 ];
 
 // function to write README file
 const writeToFile = (fileName, data) => {
-    fs.writeFile(fileName, data)
+    let file = generateMarkdown(data)
+    console.log(fileName, "\n", file)
+    
+    fs.writeFileSync(fileName, file);
 }
 
 // function to initialize program
@@ -54,10 +59,11 @@ const init = () => {
     inquirer.prompt(questions).then( response => {
         
         let fileName = response.title.split(" ").join("") + '.md'
+        
         writeToFile(fileName, response)
         
         }) 
-    }
+}
 
 // function call to initialize program
 init();
