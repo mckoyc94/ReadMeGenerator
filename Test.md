6a04662e31d655005cb673938af1d79ef687fcd3
Cmac <mckoyc@gmail.com>
2020-11-06 17:23:55 -0500

    created template for ReadMe

--- Test.md
+++ Test.md
@@ -1,3 +1,29 @@
 # Test
-  It's a test thingy
+  
+  ## Table of Contents
+  * [Description](#description)
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contribute](#contribute)
+  * [Testing](#testing)
+  * [Questions](#questions)
+    
+  ## Description
+  <p> 
+    Test
+    </p>
+  
+  ## Installation
+  <p> Testing </p>
+
+  ## Usage
+
+  ## License
+
+  ## Contribute
+
+  ## Testing
+
+  ## Questions
 
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,31 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-  ${data.description}
+  
+  ## Table of Contents
+  * [Description](#description)
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contribute](#contribute)
+  * [Testing](#testing)
+  * [Questions](#questions)
+    
+  ## Description
+  <p> Test </p>
+  
+  ## Installation
+  <p> Testing </p>
+
+  ## Usage
+
+  ## License
+
+  ## Contribute
+
+  ## Testing
+
+  ## Questions
 
 `;
 }
